d0c69b8344116f50834b512e9956c0a677cdc526
Oroxhimaru <hm51562@gmail.com>
2023-10-22 10:53:35 +0500

    useEffect

--- my-secondapp/app/[id]/page.js
+++ my-secondapp/app/[id]/page.js
@@ -1,10 +1,23 @@
-import React from 'react'
+"use client"
+
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+
+
 
 const page = ({ params }) => {
     const { id } = params;
+    const [users, setusers] = useState([])
+    const getUsers = async () => {
+      const { data } = await axios.get("https://jsonplaceholder.typicode.com/users/"+id)
+      setusers(data);
+    }
+    useEffect(() => {
+      getUsers()
+    }, [])
 
   return (
-    <div>page</div>
+    <div>{JSON.stringify(users)}</div>
   )
 }
 
